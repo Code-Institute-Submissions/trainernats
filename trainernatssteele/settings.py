b8f019802a3f4f4b935e66fb3f42a05daafc8ba7
Matthew <matthew.poyner79@gmail.com>
2021-01-26 15:45:28 +1300

    updated allowed hosts

--- trainernatssteele/settings.py
+++ trainernatssteele/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['127.0.0.1', 'trainernatssteele.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'trainernatssteele.herokuapp.com']
 
 
 # Application definition
